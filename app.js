4f25420c02365c06173eb97680b18c5540bbca26
rakaso598 <nexxus86@outlook.com>
2025-03-07 16:49:37 +0900

    Add: GET all & POST blogs method

--- app.js
+++ app.js
@@ -7,6 +7,48 @@ import { Comment } from "./models/Comment.js";
 const app = express();
 app.use(express.json());
 
+const asyncHandler = (handler) => {
+  return async (req, res) => {
+    try {
+      await handler(req, res);
+    } catch (e) {
+      switch (e.name) {
+        case "ValidationError":
+          res.status(400).send({ message: e.message });
+          break;
+        case "CastError":
+          res.status(404).send({ message: e.message });
+          break;
+        default:
+          res.status(500).send({ message: e.message });
+          break;
+      }
+      console.log(e.name);
+      console.log(e.message);
+    }
+  };
+};
+
+app.get(
+  "/blogs",
+  asyncHandler(async (req, res) => {
+    const count = req.query.count || 0;
+
+    const blogs = await Blog.find().limit(count);
+
+    res.send(blogs);
+  })
+);
+
+app.post(
+  "/blogs",
+  asyncHandler(async (req, res) => {
+    const blog = await Blog.create(req.body);
+
+    res.status(201).send(blog);
+  })
+);
+
 mongoose.connect(DATABASE_URL).then(() => console.log("Mongoose Connected!"));
 
 app.listen(3001, () => {
--- models/Comment.js
+++ models/Comment.js
@@ -2,13 +2,13 @@ import mongoose from "mongoose";
 
 const CommentSchema = new mongoose.Schema(
   {
-    title: {
-      type: String,
-    },
     content: {
       type: String,
     },
     author: {
+      type: String,
+    },
+    blogId: {
       type: String,
     },
   },
